eaa1a50eca649dfa6f641678d61139958a345b4a
Brandon Horst <brandonhorst@gmail.com>
2015-11-18 14:06:26 -0500

    Update deps

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ export default class String extends Phrase {
   describe () {
     return (
       <argument text={this.props.argument || 'string'}>
-        <freetext consumeAll={this.props.consumeAll} splitOn={this.props.splitOn} />
+        <freetext consumeAll={this.props.consumeAll} splitOn={this.props.splitOn} limit={this.props.limit} />
       </argument>
     )
   }
