33c49a4550406838081a0364723cc8eca6dc0be1
Brandon Horst <brandonhorst@gmail.com>
2015-12-11 18:05:38 -0500

    added 'trimmed' property'

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,23 @@
 /** @jsx createElement */
 import {createElement, Phrase} from 'lacona-phrase'
 
-export default class String extends Phrase {
+export default class StringPhrase extends Phrase {
+  filter (input) {
+    if (this.props.trimmed && (/^\s/.test(input) || /\s$/.test(input))) {
+      return false
+    }
+    return true
+  }
+
   describe () {
     return (
       <argument text={this.props.argument || 'string'}>
-        <freetext consumeAll={this.props.consumeAll} splitOn={this.props.splitOn} limit={this.props.limit} />
+        <freetext consumeAll={this.props.consumeAll} splitOn={this.props.splitOn} limit={this.props.limit} validate={this.filter.bind(this)} />
       </argument>
     )
   }
+}
+
+StringPhrase.defaultProps = {
+  trimmed: false
 }
